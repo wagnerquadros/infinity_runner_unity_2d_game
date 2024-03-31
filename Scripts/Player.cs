e681c3ca70411c5fee96342fc4123d40b3d18b8c
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2024-03-31 14:21:39 -0300

    Animações do PLayer

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
     private Rigidbody2D rig;
+    public Animator anim;
 
     public float speed;
     public float junpForce;
+    private bool isJumping;
 
     private void Start()
     {
@@ -22,9 +25,20 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) 
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumping) 
         {
             rig.AddForce(new Vector2(0, junpForce), ForceMode2D.Impulse);
+            anim.SetBool("jumping", true);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) //Detecta a colisão entre dois GameObjects
+    {
+        if(collision.gameObject.layer == 6)
+        {
+            anim.SetBool("jumping", false);
+            isJumping = false;
         }
     }
 }
